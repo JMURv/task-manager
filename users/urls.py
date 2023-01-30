081197e0da0a1728312661c4e433e092ec510208
JMURv <architect.lock@outlook.com>
2023-01-30 20:34:25 +0300

    fix users:URLconf file

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from .views import UsersView, UsersCreateView, UsersUpdateView, UsersDeleteView
 
 urlpatterns = [
     path('', UsersView.as_view(), name='users_list'),
-    path('create', UsersCreateView.as_view(), name='create_user'),
+    path('create/', UsersCreateView.as_view(), name='create_user'),
     path('<int:pk>/update/', UsersUpdateView.as_view(), name='update_user'),
     path('<int:pk>/delete/', UsersDeleteView.as_view(), name='delete_user'),
 ]
