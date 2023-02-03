77b6268d43b266d1632e64b4c6a892d992cb7860
JMURv <architect.lock@outlook.com>
2023-02-03 03:34:49 +0300

    Update localization

--- statuses/forms.py
+++ statuses/forms.py
@@ -10,4 +10,4 @@ class StatusForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['name'].label = _("Имя")
+        self.fields['name'].label = _("Name")
