59253521410aeeb181a1f5509f0def66fd60fb43
JMURv <architect.lock@outlook.com>
2023-02-10 03:04:47 +0300

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['task-manager.home.jmurv.site', 'webserver']
+CSRF_TRUSTED_ORIGINS = ['https://task-manager.home.jmurv.site', 'https://127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
